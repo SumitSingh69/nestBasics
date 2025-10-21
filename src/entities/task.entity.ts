a0ff8c0031b7a1429a363454b5e3a6ef4728a49e
SumitSingh69 <issomeonein1234@gmail.com>
2025-10-21 11:14:16 +0530

    added task schema

---
+++ src/entities/task.entity.ts
@@ -0,0 +1,19 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {Document} from 'mongoose';
+
+@Schema({timestamps: true})
+export class Task extends Document {
+    @Prop({required: true})
+    title : string;
+
+    @Prop({maxlength: 500})
+    description : string;
+
+    @Prop({})
+    deadLine : Date;
+
+    @Prop({default: false})
+    isCompleted : boolean;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
