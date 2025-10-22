99ac879a368581fab1b61a5b056a9e4303853c3f
SumitSingh69 <issomeonein1234@gmail.com>
2025-10-22 11:59:06 +0530

    created get and delete api

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Param } from '@nestjs/common';
 import {tasksService} from "./tasks.service";
 import { Task } from './entities/task.schema';
 import { createTaskDto } from './create-task.dto';
@@ -8,7 +8,50 @@ export class tasksController {
   constructor(private readonly tasksService: tasksService) {}
 
   @Post('/create')
-  async createTask(@Body() createTaskDto: createTaskDto) : Promise<Task> {
-    return this.tasksService.createTask(createTaskDto);
+  async createTask(@Body() createTaskDto: createTaskDto) {
+   try{ 
+    const task = await this.tasksService.createTask(createTaskDto);
+    return {
+        message: 'Task created successfully',
+        task,
+    }}
+    catch(error){
+        return {
+            message: 'Error creating task',
+            error: error.message,
+        }
+    }
+  }
+
+  @Get('/getAll') 
+  async getTasks() {
+    try{
+        const tasks = await this.tasksService.getTasks();
+        return {
+            message: 'Tasks retrieved successfully',
+            tasks,
+        }
+    }catch(error){
+        return {
+            message: 'Error retrieving tasks',
+            error: error.message,
+        }
+    }
+  }
+
+  @Delete('/delete/:id')
+  async deleteTask(@Param('id') id: string) {
+    try{
+        const result = await this.tasksService.deleteTask(id);
+        return {
+            message: 'Task deleted successfully',
+            result,
+        }
+    }catch(error){
+        return {
+            message: 'Error deleting task',
+            error: error.message,
+        }
+    }
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,15 +4,26 @@ import { Model } from 'mongoose';
 import { Task } from './entities/task.schema';
 import { createTaskDto } from './create-task.dto';
 
-
 @Injectable()
 export class tasksService {
-    constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
+  constructor(@InjectModel(Task.name) private taskModel: Model<Task>) {}
 
-    async createTask(createTaskDto: createTaskDto): Promise<Task> {
-        const newTask = new this.taskModel(createTaskDto);
-        return newTask.save();
+  async createTask(createTaskDto: createTaskDto): Promise<Task> {
+    const newTask = new this.taskModel(createTaskDto);
+    return newTask.save();
+  }
+
+  async getTasks(): Promise<Task[]> {
+    return this.taskModel.find();
+  }
+
+  async deleteTask(id: string): Promise<Task> {
+    const deletedTask = await this.taskModel.findByIdAndDelete(id);
+    if (!deletedTask) {
+      throw Error('Task not found');
     }
+    return deletedTask;
+  }
   getHello(): string {
     return 'Hello Sumit! from tasks';
   }
